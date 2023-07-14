1ba7bdc9745161dfafea3944478eed6d1dce18e8
dineshi3 <dinesh.iyyandurai@vakilsearch.com>
2023-07-14 11:06:46 +0530

    Initial commit from Create Next App

---
+++ pages/e-sign/index.js
@@ -0,0 +1,23 @@
+
+const ESignPage = (props) => {
+
+  const { embededLink } = props;
+  return (
+      <div>
+        <iframe
+          src={embededLink}
+          style={{ width: '100%', height: 'calc(100vh - 6px)', border: 'none' }}
+        />
+      </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      embededLink: `https://app.boldsign.com/document/sign/?${context.resolvedUrl.split("?")[1]}`, //responseData.signLink
+    },
+  };
+}
+
+export default ESignPage;
