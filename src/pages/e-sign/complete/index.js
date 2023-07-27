22b84ae798406ed292de4894e0b25c6bddffa903
dineshi3 <dinesh.iyyandurai@vakilsearch.com>
2023-07-27 14:41:12 +0530

    Marker Complate Page

--- src/pages/e-sign/complete/index.js
+++ src/pages/e-sign/complete/index.js
@@ -1,19 +1,12 @@
 
-const ESignPage = (props) => {
+const ESignPage = () => {
     return (
-        <div style={{textAlign: 'center'}}>
+        <h2 style={{textAlign: 'center'}}>
             Great! you have signed this document
-        </div>
+        </h2>
     );
   };
-  
-export async function getServerSideProps(context) {
-    return {
-        props: {
-            embededLink: `https://app.boldsign.com/document/sign/?${context.resolvedUrl.split("?")[1]}`, //responseData.signLink
-        },
-    };
-}
+
 
 export default ESignPage;
   
---
+++ src/pages/e-sign/marker-complete/index.js
@@ -0,0 +1,11 @@
+
+const SignMarkerCompletePage = () => {
+    return (
+        <h2 style={{textAlign: 'center'}}>
+            Great! you have signed this document. We will alert you once the document has been signed by all signatories.
+        </h2>
+    );
+  };
+  
+export default SignMarkerCompletePage;
+  
